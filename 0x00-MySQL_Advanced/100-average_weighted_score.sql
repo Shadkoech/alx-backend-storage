b84f38ed13335857e8eb23daf2f0a8b638349c9f
Shadkoech <shadrackipruto@gmail.com>
2024-03-21 20:33:45 +0300

    SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,18 @@
+-- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
+-- ComputeAverageWeightedScoreForUser  computes and store average weighted score for a student.
+
+DELIMITER ##
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+        FROM corrections
+        INNER JOIN projects
+        ON projects.id = corrections.project_id
+        WHERE corrections.user_id = user_id)
+    WHERE users.id = user_id;
+END;
+##
+
+DELIMITER ;
--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,6 +1,7 @@
 -- SQL script that creates a view need_meeting
 -- The need_meeting lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
 
+
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
     SELECT name
