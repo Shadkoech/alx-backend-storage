9b764cf5bbad3b3bd234c419a49bef5a5074fcba
Shadkoech <shadrackipruto@gmail.com>
2024-03-21 11:52:29 +0300

    Script creating users table; with a country (enumerated)

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+--  SQL script that creates a table users
+-- Attributes of the table: id, email, name, country
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
