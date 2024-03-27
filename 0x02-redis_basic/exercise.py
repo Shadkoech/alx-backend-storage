ef6a2dc3aa592b52ddc26323ae295ea984f14758
Shadkoech <shadrackipruto@gmail.com>
2024-03-27 19:16:13 +0300

    Storing lists: RPUSH, LPUSH, LRANGE, etc

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,6 +23,25 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to store history of inputs and outputs for particular
+    function
+    Args:
+        method: The function to be decorated"""
+    input_list = method.__qualname__ + ":inputs"
+    output_list = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args) -> bytes:
+        """wrapper function for hist of inputs and outputs"""
+        input = str(args)
+        self._redis.rpush(input_list, input)
+        output = method(self, *args)
+        self._redis.rpush(output_list, output)
+        return output
+    return wrapper
+
+
 class Cache:
     """A class that interact with Redis as cache
     """
@@ -34,6 +53,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data in Redis and returns the key.
         Args:
